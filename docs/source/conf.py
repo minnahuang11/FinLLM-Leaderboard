2491a14318de01b27e2800e353a7648b8bef81b9
Colin Lin <leyradc@gmail.com>
2025-01-21 20:59:43 -0500

    Reformat file structure

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,8 +6,8 @@
 # -- Path Setup --------------------------------------------------------------
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath(''))
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
